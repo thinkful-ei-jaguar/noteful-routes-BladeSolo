f3211d79c255760f6bae932038cd241012f854ef
Blade Boles <bladerboles@gmail.com>
2020-01-12 18:41:02 -0700

    Holy context, Batman!  It works.

--- solo-noteful/src/MainMain.js
+++ solo-noteful/src/MainMain.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import NotefulContext from './NotefulContext'
 
+function deleteBookmarkRequest(noteId, callback) {
+  fetch(`http://localhost:9090/notes/${noteId}`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(res => {
+      if (!res.ok) {
+        // get the error message from the response,
+        return res.json().then(error => {
+          // then throw it
+          throw error
+        })
+      }
+      return res.json()
+    })
+    .then(data => {
+      // call the callback when the request is successful
+      // this is where the App component can remove it from state
+      callback(noteId)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
 export default class MainView extends Component {
   static contextType = NotefulContext;
 
@@ -18,7 +44,15 @@ export default class MainView extends Component {
                 <li className='main-note-list' key={i}>
                   <Link to={`../../note/${note.id}`}>{note.name}</Link>
                   <p>Last modified: {note.modified.slice(0, 10)}</p>
-                  <button className='main-note-delete'>Delete Note</button>
+                  <button className='main-note-delete' onClick={() => {
+                    deleteBookmarkRequest(
+                        note.id,
+                        this.context.deleteNote
+                        )
+                      }}
+                    >
+                    Delete Note
+                  </button>
                 </li>
               )
             })}
--- solo-noteful/src/NoteMain.js
+++ solo-noteful/src/NoteMain.js
@@ -1,5 +1,32 @@
 import React, { Component } from 'react'
 import NotefulContext from './NotefulContext'
+
+function deleteBookmarkRequest(noteId, callback) {
+  fetch(`http://localhost:9090/notes/${noteId}`, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json'
+    },
+  })
+    .then(res => {
+      if (!res.ok) {
+        // get the error message from the response,
+        return res.json().then(error => {
+          // then throw it
+          throw error
+        })
+      }
+      return res.json()
+    })
+    .then(data => {
+      // call the callback when the request is successful
+      // this is where the App component can remove it from state
+      callback(noteId)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
 export default class NoteMain extends Component {
   static contextType = NotefulContext;
 
@@ -7,7 +34,8 @@ export default class NoteMain extends Component {
   render() {
     
     const { notes } = this.context;
-    const thisNote = notes.filter (note => note.id === this.props.match.params.noteid)
+    const thisNote = notes.filter (note => note.id === this.props.match.params.noteid);
+    const { history } = this.props;
     return (
       <div className="main-main-div">
 
@@ -17,7 +45,17 @@ export default class NoteMain extends Component {
                 <li className='main-note-list' key={i}>
                   <p>{note.name}</p>
                   <p>Last modified: {note.modified.slice(0, 10)}</p>
-                  <button className='main-note-delete'>Delete Note</button>
+                  <button className='main-note-delete' onClick={() => {
+                    deleteBookmarkRequest(
+                        note.id,
+                        this.context.deleteNote
+                        )
+                      
+                    history.push('/');
+                  }}
+                    >
+                    Delete Note
+                  </button>
                   <p>{note.content}</p>
                 </li>
               )
