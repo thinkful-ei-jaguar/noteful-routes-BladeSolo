2c99841ddd76d430c236b63ec5c4518ec9831d33
Blade Boles <bladerboles@gmail.com>
2020-01-15 20:42:06 -0700

    Fixed extra folders?

--- shopping-list-webpack-toPush-RobertBlade/README.md
+++
@@ -1,1 +0,0 @@
-This is a shopping list using webpack.Robert and Blade have worked on it.
--- shopping-list-webpack-toPush-RobertBlade/src/index.js
+++
@@ -1,9 +0,0 @@
-import shoppingList from './scripts/shopping-list';
-import $ from 'jquery';
-
-const main = function () {
-  shoppingList.bindEventListeners();
-  shoppingList.render();
-};
-
-$(main);
